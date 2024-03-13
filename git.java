c7ef2e097f1eadbd3d66ec7043c8c4008425b210
muni-deepesh <142885525+muni-deepesh@users.noreply.github.com>
2024-03-13 23:31:29 +0530

    Add files via upload

---
+++ git.java
@@ -0,0 +1,6 @@
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Hi mam, this is muni deepesh performing git command operations");
+        System.out.println("21MIC0147");
+    }
+}
